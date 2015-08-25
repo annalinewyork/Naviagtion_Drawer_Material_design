814453ab6ac27d7007a5398d6c72628cbfa80b8f
annalinewyork <annalinewyork@gmail.com>
2015-08-24 23:41:02 -0400

    use navi drawer with activities

---
+++ app/src/main/java/fun_house/appbar_demo1/Activity_B.java
@@ -0,0 +1,33 @@
+package fun_house.appbar_demo1;
+
+import android.os.Bundle;
+
+/**
+ * Created on 8/24/15.
+ */
+public class Activity_B extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /**
+         *  We will not use setContentView in this activty
+         *  Rather than we will use layout inflater to add view in FrameLayout of our base activity layout*/
+
+        /**
+         * Adding our layout to parent class frame layout.
+         */
+        getLayoutInflater().inflate(R.layout.activity_b, frameLayout);
+
+        /**
+         * Setting title and itemChecked
+         */
+        mDrawerList.setItemChecked(position, true);
+        setTitle(titles[position]);
+
+//        ((ImageView)findViewById(R.id.image_view)).setBackgroundResource(R.drawable.image1);
+    }
+
+
+}
